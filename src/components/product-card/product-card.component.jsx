49bedc43212b246407204f7366a6f71bcbee8632
Allan Fernandes <Fernzallan14@gmail.com>
2023-01-19 11:43:57 +0000

    cart-logic

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './product-card.styles.scss';
 import Button from '../Button/button.component'
+import { CartContext } from '../../context/CartContext';
+import { useContext } from 'react';
 
 const ProductCard = ({product}) => {
 
 const {imageUrl, name, price} = product;
 
+const {addProductToDropDown} = useContext(CartContext);
+
+const addProduct = () => addProductToDropDown(product);
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`}/>
@@ -13,7 +18,7 @@ const {imageUrl, name, price} = product;
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to Cart</Button>
+      <Button buttonType="inverted" onClick={addProduct}>Add to Cart</Button>
     </div>
   )
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,13 +1,51 @@
+import { faChessKing } from "@fortawesome/free-solid-svg-icons";
 import { createContext, useState } from "react";
+
+
+
+const addProductToDropDownHelperFn = (cartDropDownItems, productToAdd) => {
+    
+
+    //first check if item exists
+    const doesItemExist = cartDropDownItems.find(item => item.id === productToAdd.id);
+    
+
+    // if it exists, increase quantity by existing quantity + 1
+
+    
+    if(doesItemExist) {
+     return cartDropDownItems.map(item => item.id === productToAdd.id ?
+        {...item, quantity: item.quantity + 1} : {item})       
+    }
+
+
+
+
+
+
+
+
+
+    return [...cartDropDownItems, {...productToAdd, quantity:1}]
+}
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setCartOpen: () => {}
+    setCartOpen: () => {},
+    cartDropDownItems: [],
+    addProductToDropDown: () => {}
 })
 
 
 export const CartContextProvider = ({children}) => {
+
     const [isCartOpen, setCartOpen] = useState(false);
-    const value = {isCartOpen,setCartOpen}
+/*     const addProductToDropDown = (productToAdd) => addProductToDropDownHelperFn(cartDropDownItems, productToAdd); */
+    const [cartDropDownItems,setCartDropDownItems,] = useState([]);
+    const addProductToDropDown = (product) => {
+        setCartDropDownItems(addProductToDropDownHelperFn(cartDropDownItems, product))
+    }
+    
+    const value = {isCartOpen,setCartOpen,addProductToDropDown}
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
