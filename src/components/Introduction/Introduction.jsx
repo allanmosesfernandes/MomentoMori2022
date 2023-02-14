bae27f62500010b51373a2e1fe52a042ff306489
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-14 00:11:26 +0000

    type-writer-effect-complete

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -12,32 +12,38 @@ const Introduction = () => {
     
       <div className='quote-container introduction'>
       <h1>Daily Stoic Reminders</h1>
+      <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString('With roots in ancient Greece, the philosophy of Stoicism offers a unique approach to lifes challenges that promises to awaken the virtuous, resilient, and wise within you. Its teachings continue to inspire and guide people today, inviting them to embark on a journey of self-discovery and personal growth.')
+    .pauseFor(2500)
+    .typeString('<br> <br> The Stoics believed that time is one of the most valuable resources that we have, and that we should use it wisely. They believed that life is short, and that every moment counts. Therefore, they advocated for living in the present moment and focusing on what is most important.')
+      .start();
+  }} 
+  options={{
+    delay: '30',
+    pauseFor: 1000
+  }}
+  />
+    {/* <Typewriter
+    
+  options={{
+    strings: ['With roots in ancient Greece, the philosophy of Stoicism offers a unique approach to lifes challenges that promises to awaken the virtuous, resilient, and wise within you. Its teachings continue to inspire and guide people today, inviting them to embark on a journey of self-discovery and personal growth.'],
+    autoStart: true,
+    loop: false,
+    delay: 50,
+  }}
+/>
+    <Typewriter
+    options={{
+    strings: ['The Stoics believed that time is one of the most valuable resources that we have, and that we should use it wisely. They believed that life is short, and that every moment counts. Therefore, they advocated for living in the present moment and focusing on what is most important.'],
+    autoStart: false,
+    loop: false,
+    delay: 50,
+    pauseFor: 100000
 
-      <Typewriter
-        onInit={(typewriter) => {
-        typewriter.typeString(`With roots in ancient Greece, the philosophy of Stoicism offers a unique approach to life's challenges that promises to awaken the virtuous, resilient, and wise within you. Its teachings continue to inspire and guide people today, inviting them to embark on a journey of self-discovery and personal growth.`)
-          .callFunction(() => {
-            this.secondTypewriter.start();
-          })
-          .start();
-        }}
-        options={{
-          delay: 50,
-        }}
-      />
-      <Typewriter
-        onInit={(typewriter) => {
-          this.secondTypewriter = typewriter;
-        }}
-        ref={(typewriter) => {
-          this.secondTypewriter = typewriter;
-        }}
-        options={{
-          delay: 50,
-        }}
-      >
-        The Stoics believed that time is one of the most valuable resources that we have, and that we should use it wisely. Stoics believed life to be fleeting and every moment valuable. They encouraged individuals to embrace the present moment and concentrate on what matters, rather than waste time on insignificant pursuits.
-      </Typewriter>
+  }}
+/> */}
+
 
 
       <div className="scroll-down">
