9ee0bca416ec1e7d4878080d6b7805d17ebc42e8
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-03 15:11:52 +0100

    new-firebase-utils

---
+++ src/utils/firebaseUtils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkLC_hTNKO6NeFllc6iIK6NVzEtnwjMUI",
+  authDomain: "momento-mori-redux.firebaseapp.com",
+  projectId: "momento-mori-redux",
+  storageBucket: "momento-mori-redux.appspot.com",
+  messagingSenderId: "674007536364",
+  appId: "1:674007536364:web:544da12042332a36ff8bc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
