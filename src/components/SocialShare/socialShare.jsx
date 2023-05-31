3b5bbc020e86ccabaa2575d6244e2343f5e892b2
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-31 13:01:10 +0100

    Clipboard Copy init

--- src/components/SocialShare/socialShare.jsx
+++ src/components/SocialShare/socialShare.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SocialsContainer } from './StyledSocials';
 import ShareIcon from "../../assets/images/Share-icon.svg";
 
 const SocialShare = () => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyLink = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000);
+  }
+
+
+
   return (
     <SocialsContainer className='quote-container'>
       <div>
         <h2>Share: <img src={ShareIcon} alt="Share Icon"/></h2>
       </div>
       <div>
-        
+        <ul>
+          <li>
+            <p className="number">01</p>
+            <p onClick={handleCopyLink}>
+              {copied ? "Copied!" : "Copy Link"}
+            </p>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
     </SocialsContainer>
   )
