cd32256b5c1e6c4e85409460df10303e06b1e08f
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-31 13:56:04 +0100

    Share-hover-effect

--- src/components/SocialShare/StyledSocials.js
+++ src/components/SocialShare/StyledSocials.js
@@ -4,10 +4,50 @@ export const SocialsContainer = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr;
 gap: 1rem;
+font-family: neue-bold, monospace;
 
 h2 {
     display: flex;
-    font-size: 2.5rem;
+    font-size: 3.5rem;
     gap: 1rem;
+    justify-content: center;
 }
+`
+
+
+export const StyledList = styled.ul`
+li {
+    display: flex;
+    gap: 3rem;
+    transition: transform 2000ms ease-in-out;
+    transition-delay: 0.1s;
+    border-bottom: 1px solid white;
+    align-items: center;
+    button, p, img{
+        transition: transform 200ms ease-in-out;
+    }
+    img {
+        margin-left: auto;
+    }
+
+    p:nth-child(2) {
+        cursor: pointer;
+    }
+    &:hover p:nth-child(2){
+        transform: translateX(5px);
+    }
+
+    &:hover button {
+        transform: translateX(5px);
+    }
+
+    &:hover img{
+        transform: rotate(45deg);
+        border: 1px solid white;
+        border-radius: 50%;
+        width: 20px;
+        height: 20px;
+    }
+}
+
 `
--- src/components/SocialShare/socialShare.jsx
+++ src/components/SocialShare/socialShare.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
-import { SocialsContainer } from './StyledSocials';
+import { SocialsContainer, StyledList } from './StyledSocials';
 import ShareIcon from "../../assets/images/Share-icon.svg";
+import Plus from "../../assets/images/plus.svg";
+import clipboardCopy from 'clipboard-copy';
+import { WhatsappShareButton, TwitterShareButton, LinkedinShareButton} from 'react-share';
 
 const SocialShare = () => {
   const [copied, setCopied] = useState(false);
-
+  const shareURL = "https://dailystoicreminders.uk/";
   const handleCopyLink = () => {
+    const url = 'https://dailystoicreminders.uk/';
+    clipboardCopy(url);
+
     setCopied(true);
     setTimeout(() => {
       setCopied(false)
@@ -20,17 +26,37 @@ const SocialShare = () => {
         <h2>Share: <img src={ShareIcon} alt="Share Icon"/></h2>
       </div>
       <div>
-        <ul>
+        <StyledList>
           <li>
             <p className="number">01</p>
             <p onClick={handleCopyLink}>
               {copied ? "Copied!" : "Copy Link"}
             </p>
+            <img src={Plus} alt="Plus Sign" />
           </li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+          <li>
+            <p className="number">02</p>
+            <WhatsappShareButton url={shareURL}>
+              Whatsapp
+            </WhatsappShareButton>
+            <img src={Plus} alt="Plus Sign" />
+          </li>
+          <li>
+            <p className="number">03</p>
+            <TwitterShareButton url={shareURL}>
+              Twitter
+            </TwitterShareButton>
+            <img src={Plus} alt="Plus Sign" />
+          </li>
+          <li>
+            <p className="number">04</p>
+              <LinkedinShareButton url={shareURL}>
+                Linkedin
+              </LinkedinShareButton>
+            <img src={Plus} alt="Plus Sign" />
+          </li>
+
+        </StyledList>
       </div>
     </SocialsContainer>
   )
