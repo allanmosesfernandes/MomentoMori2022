f5d23412a076e25f6414f935cb98ce23db2c5a2b
allanmosesfernandes <fernzallan14@gmail.com>
2025-07-03 09:41:08 +0100

    build

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,6 @@ root.render(
     <BrowserRouter>
     {/* <ProductsProvider> */}
       {/* <UserProvider> */}
-      
         <CartContextProvider>
           <App />
         </CartContextProvider>
